a2af7bfd031675b89ddc671dd08b3290de96876e
Eugene <eugene.beloff@gmail.com>
2014-07-10 02:15:45 +0300

    add mouse handler with action&id's

--- pure-project/app/src/scripts/main.js
+++ pure-project/app/src/scripts/main.js
@@ -12,17 +12,21 @@ $(function(){
 			$.each(this.productsList, function(index, obj) {
 				readyTpl += template.replace(/{{title}}/ig, obj.title)
 									.replace(/{{sku}}/ig, obj.sku)
-									.replace(/{{price}}/ig, obj.price);
+									.replace(/{{price}}/ig, obj.price)
+									.replace(/{{prodId}}/ig, index);
 			});
 
 			$("#products-table > tbody").append(readyTpl);
+			$('.actions > .btn').click(function() {
+				console.log($(this).attr('data-action'));
+			})
+			
 		},
 		fetch: function() {
 			var self = this;
 
 			$.getJSON(this.url, function(data) {
 				self.productsList = data.products;
-				
 				self.attachTemplate();
 			});
 		}
