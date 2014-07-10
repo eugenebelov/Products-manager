8dfc9b9d95e2e303433b1eb19c3d7ff9aef9f3b4
Eugene <eugene.beloff@gmail.com>
2014-07-11 01:45:13 +0300

    add hash func

--- pure-project/app/src/scripts/main.js
+++ pure-project/app/src/scripts/main.js
@@ -1,44 +1,112 @@
 $(function(){
-	var Products = {
-		init: function() {
-			this.url = "./src/fake/data.js";
+	$(window).ready(function() {
+		 Hash.set("page", "preview");
+	});
 
+	$(window).on('hashchange', function() {
+	  var page = Hash.get("page");
+	  if(main[page]) main[page]();
+
+	});
+
+	var main = {
+		preview:function () {
+			Products.get(function(data){
+				Views.render('list', data, ["title", "sku", "price", "prodId"]);
+				//console.log(data)
+			})	
+		},
+		edit:function () {
+			//Products.get(function(data){
+				console.log('edit');
+			//})	
+		}
+	}
+
+	var Views = {
+		getView: function(viewName, callback) {
+			var tpl = '';
+			$.ajax({
+			  url: "src/scripts/templates/" + viewName + ".html",
+			  async: false,
+			  cache: false
+			})
+			.done(function( html ) {
+				tpl = html;
+				if(callback)
+					callback(html);
+
+			});
+			return tpl;
+		},
+
+		render: function(view, data, params) {
+			var template = this.getView(view);
+			var tmpTeplate = template;
+			var result = [];
+			$.each( data, function( i, dataObj ) {
+				tmpTeplate = template;
+			 	$.each( params, function( j, dataParam ) {
+			 		console.log(dataObj, dataParam);
+			 		tmpTeplate = tmpTeplate.replace('{{'+dataParam+'}}', dataObj[dataParam]);
+			 	});
+			 	result.push(tmpTeplate);
+
+			});
+			console.log(result);
+			//for(var i = 0, l = params.length; i < l; i++){
+			//	if()	
+			//}
+			// console.log(template);
+		}
+	}
+
+	var Products = {
+		path:"src/scripts/fake/data.json",
+		init: function() {
 			this.fetch();
 		},
-		attachTemplate: function() {
-			var template = $.trim($("#products-table-tpl").html()),
-				readyTpl = '';
+		attachTemplate: function(tpl) {
+			var template = tpl,
+				$prodBody = $("#products-table-body");
+				readyTpl = [];
 
 			$.each(this.productsList, function(index, obj) {
-				readyTpl += template.replace(/{{title}}/ig, obj.title)
+				readyTpl[readyTpl.length] = template.replace(/{{title}}/ig, obj.title)
 									.replace(/{{sku}}/ig, obj.sku)
 									.replace(/{{price}}/ig, obj.price)
 									.replace(/{{prodId}}/ig, index);
 			});
 
-			$("#products-table > tbody").append(readyTpl);
-			$('.actions > .btn').click(function() {
+			$prodBody.html(readyTpl.join(""));
+			$prodBody.find('.actions > .btn').click(function() {
 				console.log($(this).attr('data-action'));
 			})
 			
+			$('#view').append(template);
 		},
 		fetch: function() {
 			var self = this;
 
-			$.getJSON(this.url, function(data) {
+			$.getJSON(this.path, function(data) {
 				self.productsList = data.products;
-				self.attachTemplate();
+				Views.getView("list", $.proxy( Products.attachTemplate, Products));
 			});
 		},
-		get: function() {
-			return this.productsList;
+		get: function(callback) {
+			var self = this;
+			if (!self.productsList)
+			{
+				$.getJSON(this.path, function(data) {
+					self.productsList = data.products;
+					callback(data.products);
+				});
+			} else {
+				callback(self.productsList);
+			}
 		}
 	};
 
-	Products.init();
+	//Products.init();
 
-	$(window).on('onhashchange', function(){
-		// Alerts every time the hash changes!
-		console.log( location.hash );
-	})
 });
