6744fd4fd1c2c76947e14a0d7f1cee085ac34f64
Eugene <eugene.beloff@gmail.com>
2014-07-18 21:48:58 +0300

    add backbone experiments

--- modern-project/app/scripts/main.js
+++ modern-project/app/scripts/main.js
@@ -1,70 +1,130 @@
-ProductApp = new Backbone.Marionette.Application();
+var ProductApp = new Backbone.Marionette.Application();
 
 ProductApp.addRegions({
-	mainRegion: "#view"
+	productListRegion: "#view",
+	exportRegion: "#viewExported"
 });
 
-Product = Backbone.Model.extend({
-	title: 'test-title',
-	sku: '1',
-	price: '19.99'
-});
+// var productController = new ProductController();
+// productController.AddProductList({region: ProductApp.productListRegion});
 
-ProductsCollection = Backbone.Collection.extend({
-	model: Product
-});
-
-ProductItemView = Backbone.Marionette.ItemView.extend({
-	template: '<tr>
-				  <td><%= title %></td>
-				  <td><%= sku %></td>
-				  <td><%= price %></td>
-				  <td class="actions">
-				      <div class="btn btn-primary" onclick="productModel.onEdit(<%= sku %>)">edit</div>
-				      <div class="btn btn-warning" onclick="productModel.onRemove(<%= sku %>)">delete</div>
-				  </td>
-				</tr>',
-	tagName: 'tbody'
-});
-
-ProductsView = Backbone.Marionette.CompositeView.extend({
-  tagName: "table",
-  id: "products-table",
-  className: "table",
-  template: '<table>
-			  <thead>
-			    <tr>
-			      <th>Title</th>
-			      <th>Sku</th>
-			      <th>Price</th>
-			      <th>Actions</th>
-			    </tr>
-			  </thead>
-			  <tbody id="products-table-body">
-			    {{items}}
-			  </tbody>
-			</table>',
-  itemView: ProductItemView,
+ProductApp.module('ProductApp', function(module, App, Backbone, Marionette, $, _) {
+	/* definition for book model, with default example of data structure */
+    module.BookModel = Backbone.Model.extend({
+        defaults: {
+            title: '',
+            sku: '',
+            price: ''
+        }
+    });
  
-  appendHtml: function(collectionView, itemView) {
-    collectionView.$("tbody").append(itemView.el);
-  }
-});
-
-ProductApp.addInitializer(function(options){
-  var productsView = new ProductsView({
-    collection: options.prod
-  });
-
-  ProductApp.mainRegion.show(productsView);
-});
-
-$(document).ready(function(){
-  var products = new ProductsCollection([
-    { title: 'P1', sku:'1', price:'19.99' },
-    { title: 'P2', sku:'2', price:'19.99' },
-    { title: 'P3', sku:'3', price:'19.99' }
-  ]);
+    /* definition for book collection */
+    module.BookCollection = Backbone.Collection.extend({
+         
+        /* set model type used for this collection */
+        model: module.BookModel,
  
-  ProductApp.start({prod: products});
+        /* comparator determines how collection is sorted */
+        comparator: 'authorLast',
+
+        url: 'scripts/fake/data.json'
+    });
+ 
+    /* definition for individual item view */
+    module.BookItemView = Marionette.ItemView.extend({
+        tagName: 'tr',
+ 
+        /* set the template used to display this view */
+        template: '#product-list-template',
+ 
+        /* used to show the order in which these method are called */
+        initialize: function(){ console.log('BookItemView: initialize >>> ' + this.model.get('title')) },
+        onRender: function(){ console.log('BookItemView: onRender >>> ' + this.model.get('title')) },
+        onShow: function(){ console.log('BookItemView: onShow >>> ' + this.model.get('title')) }
+    });
+ 
+    /* definition for collection view */
+    module.BookCollectionView = Marionette.CollectionView.extend({
+        tagName: "tbody",
+        /* explicitly set the itemview used to display the models in this collection */
+        // itemView: module.BookItemView,
+        childView: module.BookItemView,
+ 
+        initialize: function(){ console.log('BookCollectionView: initialize') },
+        onRender: function(){ console.log('BookCollectionView: onRender') },
+        onShow: function(){ console.log('BookCollectionView: onShow') }
+    });
+ 
+    /* define a view; in this case a 'Layout' */
+    module.AppLayoutView = Backbone.Marionette.LayoutView.extend({
+         
+        /* the auto-generated element which contains this view */
+        tagName: 'table',
+ 
+        /* id attribute for the auto-generated container element */
+        id: 'products-table',
+        className: 'table',
+ 
+        /* reference to the template which will be rendered for this view */
+        template: '#product-list-body-template',
+ 
+        /* define the regions within this layout, into which we will load additional views */
+        regions: {
+            RegionOne : '#products-table-body'
+        },
+ 
+        /* called when the view initializes, before it displays */
+        initialize: function() {
+            console.log('main layout: initialize');
+        },
+ 
+        /* called when view renders in the DOM. This is a good place to 
+            add nested views, because the views need existing DOM elements
+            into which to be rendered. */
+        onRender: function() {
+            console.log('main layout: onRender');
+             
+            /* create an array of books using anonymouse objects;
+                the objects have the same structure as in the 'defaults'
+                attribute of the module.BookModel definition */
+            var bookArray = [];
+
+            // bookArray.push({title: 'Wolf',sku: 'Harrison', price: 'Jim'});
+            // bookArray.push({title: 'The Theory and Practice of Rivers', sku: 'Snyder', price: 'Gary'});
+            // bookArray.push({title: 'Weather Central',sku: 'Kooser', price: 'Ted'});
+            // bookArray.push({title: 'Losing Season',sku: 'Ridl', price: 'Jack'});
+            // bookArray.push({title: 'Mornings Like This',sku: 'Dillard', price: 'Annie'});
+ 
+            /* create a collection using the array of anonymous objects */
+            var bookCollection = new module.BookCollection(bookArray);
+            
+            bookCollection.fetch({
+	            success: function (prod) {
+	                bookArray = _.clone(bookCollection.models[0].attributes.products);
+	                console.log(bookCollection);
+	            }
+	        });
+ 
+            /* create new instance of the collection view using the bookCollection */
+            var bookCollectionView = new module.BookCollectionView({collection: bookCollection});
+ 
+            /* display the collection view in region 1 */
+            this.RegionOne.show(bookCollectionView);
+        },
+ 
+        /* called when the view displays in the UI */
+        onShow: function() {
+            console.log('main layout: onShow');
+        }
+    });
+
+	module.addInitializer(function(){
+        /* create a new instance of the layout from the module */
+        var layout = new module.AppLayoutView();
+ 
+        /* display the layout in the region defined at the top of this file */
+        ProductApp.productListRegion.show(layout);
+    });
 });
+
+$(document).ready(function() {ProductApp.start();});
---
+++ modern-project/app/scripts/model.js
@@ -0,0 +1,13 @@
+var Product = Backbone.Model.extend({
+    defaults: {
+        title: "Title",
+        sku: "11",
+        price: "199.99"
+    }
+});
+
+var ProductsCollection = Backbone.Collection.extend({
+    model: Product,
+
+    url: 'scripts/fake/data.json'
+});
